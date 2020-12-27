6a78f6b7a832328728247d4d09286c499d709c1f
David Berens <David.Berens@thyssenkrupp.com>
2020-12-27 19:17:12 +0100

    Fixed Navbar

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -4,6 +4,7 @@
       <div class="container-fluid">
         <a class="navbar-brand" href="#">Navbar</a>
         <button
+          id="navbar-button"
           class="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
@@ -16,11 +17,13 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <li @click="toggleNavbar" class="nav-item">
+              <router-link class="nav-link" to="/">Home</router-link>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
+            <li @click="toggleNavbar" class="nav-item">
+              <router-link class="nav-link" to="/incident"
+                >Verbandsbucheintrag</router-link
+              >
             </li>
             <li class="nav-item dropdown">
               <a
